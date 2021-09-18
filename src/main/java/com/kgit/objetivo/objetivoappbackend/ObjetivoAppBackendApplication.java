d9745596d3bb06474e5d85f33545db94963fa5c4
Kamil Golabek <kamgolabek@gmail.com>
2021-09-18 16:25:39 +0200

    init project

---
+++ src/main/java/com/kgit/objetivo/objetivoappbackend/ObjetivoAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kgit.objetivo.objetivoappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjetivoAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ObjetivoAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kgit/objetivo/objetivoappbackend/ObjetivoAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kgit.objetivo.objetivoappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjetivoAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
